3a03e04518e08ecd18e15aa5801ad1cb444790ef
Kegan Overberg <overbergkegan284@Gmail.com>
2021-11-15 23:59:09 +0200

    created how-we-work page

---
+++ src/pages/how-we-work.jsx
@@ -0,0 +1,24 @@
+// React imports.
+import * as React from "react";
+
+// Component imports.
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+const HowWeWork = () => {
+    return(
+        <div>
+
+            <Navbar />
+
+            
+
+            <Footer />
+
+        </div>
+    );
+}
+
+
+export default HowWeWork;
